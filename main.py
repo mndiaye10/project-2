5bc361a8439507c89127e73c56b0b23d41501323
Moustapha Ndiaye <moustapha199410@gmail.com>
2021-03-25 21:59:21 +0000

    this not bette

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 notes=[]
 effectifs=[]
 val_eff=[]
-nombre=0
+nombre=20
 n=0
 i=0
 while n<=20:
-  n=input("veuillez  saisir une valeur de la série ou écrire une valeur négatif pour arréter le programme: \n")
+  n=input("veuillez  saisir une valeur  ou écrire une valeur négatif pour arréter le programme: \n")
   if n!=0:
     n= float(n)
     notes.append(n)
@@ -15,7 +15,7 @@ while n<=20:
       effectifs.append(n)
       for i in range (len(notes)) :
         val_eff.append(notes[i]*effectifs[i])
-        m= sum(val_eff)/sum(effectifs)
+        m= sum(val_eff)/sum(effectifs )
         print("la liste des notes des éléves est:",notes)
         print("le nombre de note de cette liste est:",nombre)
         print("la note minimale de la liste est:",min(notes))
