ecabc93056c17a61eee23054708066e4f7d1e509
Moustapha Ndiaye <moustapha199410@gmail.com>
2021-03-25 03:13:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+notes=[]
+effectifs=[]
+val_eff=[]
+nombre=0
+n=0
+i=0
+while n<=20:
+  n=input("veuillez  saisir une valeur de la série ou écrire une valeur négatif pour arréter le programme: \n")
+  if n!=0:
+    n= float(n)
+    notes.append(n)
+    for i in range (len(notes)) :
+      print("entrer l'effectif correspondant à la notes[i]")
+      n=int(input())
+      effectifs.append(n)
+      for i in range (len(notes)) :
+        val_eff.append(notes[i]*effectifs[i])
+        m= sum(val_eff)/sum(effectifs)
+        print("la liste des notes des éléves est:",notes)
+        print("le nombre de note de cette liste est:",nombre)
+        print("la note minimale de la liste est:",min(notes))
+        print("la note maximale de la liste est:",max(notes))
+        print("la moyenne est",m)
